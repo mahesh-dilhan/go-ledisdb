e88cd9f1783c6fa557b5bb6f70e71c8162d3b793
mahesh <mail.mahesh.lk@gmail.com>
2021-07-25 08:54:53 +0800

    ledisdb-go-connect-client

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	lediscfg "github.com/ledisdb/ledisdb/config"
+	"github.com/ledisdb/ledisdb/ledis"
+)
+
+func main() {
+
+	// Use Ledis's default config
+	cfg := lediscfg.NewConfigDefault()
+	l, _ := ledis.Open(cfg)
+	db, _ := l.Select(0)
+
+	db.Set(key, value)
+
+	db.Get(key)
+}
